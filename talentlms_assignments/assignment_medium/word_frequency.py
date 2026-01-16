75f054fe0adbef62eca394a8f98583bbaf4347d3
iNoumanMalik <inoumanmalik@outlook.com>
2026-01-16 13:12:07 +0500

    Added word frequence task

---
+++ talentlms_assignments/assignment_medium/word_frequency.py
@@ -0,0 +1,28 @@
+def find_word_freq(data):
+    words = data.split()
+    # print(words)
+    count = 0
+    frequencyDict = {}
+    for x in words:
+        x = x.strip(".")
+        for y in words:
+            y = y.strip(".")
+            if x == y:
+                count += 1
+                
+                
+        frequencyDict[x] = count
+        count = 0
+        
+    return frequencyDict
+
+
+with open("demotext.txt","w") as f:
+    f.write("I am Nouman. I am from Islamabad. Islamabad is a beautiful city.")
+    
+with open("demotext.txt") as f: #rt is default that's why we don't usually write
+    data = f.read()
+    # print(data)
+
+print(find_word_freq(data))
+
