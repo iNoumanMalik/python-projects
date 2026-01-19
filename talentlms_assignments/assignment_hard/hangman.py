3f4d8635e776118573d58bc8378ca1b34b5c40a2
iNoumanMalik <inoumanmalik@outlook.com>
2026-01-19 15:44:23 +0500

    Refactored and optimized the code

--- talentlms_assignments/assignment_hard/hangman.py
+++ talentlms_assignments/assignment_hard/hangman.py
@@ -2,80 +2,62 @@ import random
 
 def random_secret():
     SECRET_WORDS = ["cinema","dress","keyboard","fantastic","enthusiastic"]
-    random_integer = random.randint(0,len(SECRET_WORDS))
-    return SECRET_WORDS[random_integer]
+    return random.choice(SECRET_WORDS)
+    # random_integer = random.randint(0,len(SECRET_WORDS)-1)
+    # return SECRET_WORDS[random_integer]
 
+def display_progress(guessed_letters, secret_word):
+    print("\n")
+    for char in secret_word:
+        print(char if char in guessed_letters else "_", end=" ")
+    print("\n\n____GUESS THE WORD____")
+        
 
-def user_guess_word(random_word, attempts):  
+def play_game(secret_word, attempts):  
     guessed_letters = set()
     wrong_guessed = set()
-    finish = False
+    
+    display_progress(guessed_letters, secret_word)
+    
     while attempts > 0:
         print(f"Remaining attempts: {attempts}")   
-        user_guess = input("Your choice: ").lower()
+        user_guess = input("Enter a letter: ").lower()
         
         if len(user_guess) != 1 or not user_guess.isalpha():
-            print("Enter one character!")
+            print("Enter one alphabet character!")
             continue
         
-        if user_guess in random_word and user_guess in guessed_letters:
+        if user_guess in guessed_letters or user_guess in wrong_guessed:
             print("You already guessed this word")
+            continue
         
-        if user_guess in random_word:
+        if user_guess in secret_word:
             guessed_letters.add(user_guess)
             print("Correct")
-            handle_display(guessed_letters, random_word)
         else:
+            wrong_guessed.add(user_guess)
+            attempts = attempts - 1
             print("Wrong")
-            if user_guess not in wrong_guessed:
-                attempts = attempts - 1
-                wrong_guessed.add(user_guess)
-            else:
-                print("You are guessing the wrong word again")
+
+        display_progress(guessed_letters, secret_word)
         
-        if guessed_letters != set(random_word):
-            continue
-        break   
-
+        if set(secret_word) == guessed_letters: # we compare same elements even ordering is changed, it does not affect
+            print("\nYou WIN! The word was:", secret_word)
+            return True
     
-    if guessed_letters == set(random_word): # we compare same elements even ordering is changed, it does not affect
-        print("\nYou win... Hurray")
-        choice = input("Replay? Enter Y/N to continue: ").lower()
-        if(choice == 'y'):
-            finish = True
-    
-    if attempts == 0:
-        print("\nYou lost ... Sad!")
-        choice = input("Replay? Enter Y/N to continue: ").lower()
-        if(choice == 'y'):
-            finish = True
-        
-    return finish
-
-
-def handle_display(guessed_letters, random_word):
-    display_progress(guessed_letters, random_word)
-    
-    
-def display_progress(guessed_letters, random_word):
-    print("\n")
-    for char in random_word:
-        print(char if char in guessed_letters else "_", end=" ")
-    if(guessed_letters != set(random_word)):
-        print("\n\n____GUESS THE WORD____")
-    
-    
-    
+    print("\nYou LOST! The word was:", secret_word)
+    return False
 
 def main():
     ATTEMPTS = 5
-    random_word = random_secret()
-    guessed_letters = set()
-    display_progress(guessed_letters,random_word)
-    replay = user_guess_word(random_word,ATTEMPTS)
-    if replay:
-        main()
-        
     
-
+    while True:
+        secret_word = random_secret()
+        play_game(secret_word,ATTEMPTS)
+        
+        choice = input("\nReplay? (y/n)").lower()
+        if choice !="y":
+            print("Thanks for playing")
+            break   
+    
 main()
